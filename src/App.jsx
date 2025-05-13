5067655d98130ce1e2d921203fe87b135007fda2
treina_01 <treina_01@cicc.local>
2025-05-13 15:03:26 -0300

    primeiro commit

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+
+
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <header>
+        <h1>Thiago de Assis</h1>
+        <p>Aluno do CADSPM</p>
+      </header>
+       <main>
+        <h2>Sobre mim</h2>
+        <p>Lorem ipsum dolor sit amet <strong>consectetur adipisicing elit. </strong>Perspiciatis voluptatum tempora 
+          corporis reiciendis repudiandae sed ut temporibus labore tempore expedita natus rem consequatur 
+          minima nemo vero, beatae voluptate sequi recusandae.</p>
+
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+             Quas laborum consequuntur recusandae obcaecati
+             cum unde explicabo corporis magni adipisci odio ea rem, quaerat
+              aliquam placeat praesentium deserunt neque fugit esse?</p>
+              <h3>Habilidades Principais</h3>
+              <ul>
+                <li>Montagem e Manutenção de PC</li>
+                <li>Redes e Conectividade</li>
+                <li>Sistemas operacionais (Windows, Linux e MS-DOS)</li>
+                <li>Didática técnica para cursos e certificações</li>
+              </ul>
+
+       </main>
+
+      <footer>
+      &copy; Thiago de Assis  - Todos os direitos reservados
+        
+      </footer>
+    </>
+  )
+}
+
+export default App
